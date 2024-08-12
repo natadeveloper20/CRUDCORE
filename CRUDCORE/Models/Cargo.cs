e6d7cbdf2eeb33b2095b547a3946bd36a1c87311
natadeveloper20 <mendietanatanael9@gmail.com>
2024-08-12 18:40:18 -0300

    Agregar archivos de proyecto.

---
+++ CRUDCORE/Models/Cargo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDCORE.Models
+{
+    public partial class Cargo
+    {
+        public int Idcargo { get; set; }
+        public string? Descripcion { get; set; }
+
+        public virtual ICollection<Empleado> Empleados { get; set; }
+    }
+}
---
+++ CRUDCORE/Models/DBCRUDCOREContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CRUDCORE.Models
+{
+    public partial class DBCRUDCOREContext : DbContext
+    {
+        public DBCRUDCOREContext()
+        {
+        }
+
+        public DBCRUDCOREContext(DbContextOptions<DBCRUDCOREContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cargo> Cargos { get; set; } = null!;
+        public virtual DbSet<Empleado> Empleados { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cargo>(entity =>
+            {
+                entity.HasKey(e => e.Idcargo)
+                    .HasName("PK__CARGO__3CE7FC2D5E00CFD8");
+
+                entity.ToTable("CARGO");
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Empleado>(entity =>
+            {
+                entity.HasKey(e => e.IdEmpleado)
+                    .HasName("PK__EMPLEADO__CE6D8B9E8B88E5EA");
+
+                entity.ToTable("EMPLEADO");
+
+                entity.Property(e => e.Correo)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NombreCompleto)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Telefono)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.oCargo)
+                    .WithMany(p => p.Empleados)
+                    .HasForeignKey(d => d.Idcargo)
+                    .HasConstraintName("FK_cargo");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
